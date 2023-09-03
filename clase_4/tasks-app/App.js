5b527c908b7bb0e3bb4776940d2462d08d3e9ba9
Profe Pablo <pablo-macia@live.com.ar>
2023-09-03 20:20:56 -0300

    Clase 4 - Finalizada

--- clase_4/tasks-app/App.js
+++ clase_4/tasks-app/App.js
@@ -1,9 +1,12 @@
-import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
-import { useEffect, useState } from 'react';
+import { StyleSheet, TextInput, View, Button, Text, FlatList, Modal } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
   const [textItem, setTextItem] = useState('')
   const [itemList, setItemList] = useState([])
+
+  const [itemSelected,setItemSelected] = useState({})
+  const [isModalVisible,setIsModalVisible] = useState(false)
 
   const onChangeTextHandler = (text) => {
     setTextItem(text)
@@ -16,7 +19,26 @@ export default function App() {
     setTextItem('')
   }
 
-  useEffect(()=>console.log(textItem ),[textItem]) //Considerar si usarlo en la clase 4 o no.
+  const onSelectItemHandler = (id) => {
+    setIsModalVisible(!isModalVisible)
+    setItemSelected(itemList.find((item)=>item.id===id))
+  }
+
+  const onDeleteItemHandler = () => {
+    setItemList(itemList.filter((item)=> item.id!==itemSelected.id))
+    setItemSelected({})
+    setIsModalVisible(!isModalVisible)
+  }
+
+  const renderListItem = ({item}) => {
+    return(
+      <View style={styles.listItem}>
+        <Text>{item.value}</Text>
+        <Button onPress={()=>onSelectItemHandler(item.id)} title='x' />
+      </View>
+    )
+  }
+
 
   return (
     <View style={styles.container}>
@@ -29,9 +51,33 @@ export default function App() {
           />
         <Button onPress={addItemToList} title="Add" />
       </View>
-      <View>
+       {/* <View>
         {itemList.map(item => <View key={item.id} style={itemList}><Text>{item.value}</Text></View>)}
-      </View>
+        </View>  */}
+
+        <FlatList
+          data={itemList}
+          renderItem={renderListItem}
+          keyExtractor={item=>item.id}
+        />
+        <Modal animationType='slide' visible={isModalVisible}>
+          <View style={styles.modalMessageContainer}>
+            <Text>Se eliminará la tarea:</Text>
+            <Text>{itemSelected.value}</Text>
+          </View>
+          <View style={styles.modalButtonContainer}>
+            <Button
+              title="Eliminar"
+              onPress={()=>onDeleteItemHandler(itemSelected)}
+              color='#ef233c'
+            />
+            <Button
+              title="Cancelar"
+              onPress={()=>setIsModalVisible(!isModalVisible)}
+              color='#8d99ae'
+            />
+          </View>
+        </Modal>
     </View>
   );
 }
@@ -50,5 +96,24 @@ const styles = StyleSheet.create({
     borderBottomColor: '#ccc',
     borderBottomWidth:1,
     width:300,
+  },
+  listItem: {
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems: 'center',
+    padding:10,
+    margin:4,
+    backgroundColor:'#a2d2ff',
+    borderRadius: 10,
+  },
+  modalMessageContainer:{
+    paddingTop:50,
+    alignItems: 'center',
+  },
+  modalButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingTop: 20,
   }
+
 });
